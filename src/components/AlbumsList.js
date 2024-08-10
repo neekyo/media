3ef6d900eea6053525504d4612bd12c9f156bf43
neekyo <codebyns@gmail.com>
2024-08-10 14:39:18 -0400

    RTK Query API & Albums List component set up

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,11 @@
+import { useFetchAlbumsQuery } from '../store';
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+	const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+	console.log(data, error, isLoading);
+
+	return <div>Albums for {user.name}</div>;
 }
 
 export default AlbumsList;
